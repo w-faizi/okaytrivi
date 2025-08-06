aae691bc6a4ee4b0fbac0cf0b78caae7085f0f64
Zishan Faizi <88134937+w-faizi@users.noreply.github.com>
2025-08-07 04:21:57 +0530

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import TriviaGame from './components/TriviaGame';
+
+function App() {
+  return (
+    <div className="App">
+      <TriviaGame />
+    </div>
+  );
+}
+
+export default App;
